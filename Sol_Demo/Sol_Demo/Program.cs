33942ab00e4ae560a586f6f381baf9e36758d770
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-16 16:46:13 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Sol_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Demo demo = new Demo();
+
+            // Compile time error 
+            //demo.FullName = "Kishor Naik"; // read Only property
+            Console.WriteLine(demo.FullName);
+            demo.Age = 35;
+            Console.WriteLine(demo.Age);
+
+            demo.DemoMethod("Kishor","Naik");
+
+            Console.WriteLine(demo.DemoMethodReturn("Eshaan", "Naik"));
+
+           
+        }
+    }
+
+    public class Demo
+    {
+        // Constructor
+        public Demo() => Console.WriteLine("Constructor Call");
+
+        // Property
+        // Read Only Property
+        public String FullName => "Kishor Naik";
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        // Method
+        public void DemoMethod(string firstName, string lastName) => Console.WriteLine($"{firstName} {lastName}");
+
+        public String DemoMethodReturn(string firstName, string lastName) => $"{firstName} {lastName}";
+
+    }
+}
